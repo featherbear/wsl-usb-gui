c57d70988062fea8de3826cefdb444290283c814
Andrew Leech <andrew.leech@planetinnovation.com.au>
2022-04-07 11:11:44 +1000

    MSI build updates.

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 #
 # SPDX-License-Identifier: MIT
 # coding=utf-8
+import os
 from setuptools import setup, find_packages
 from setuptools.command.sdist import sdist as _sdist
 import subprocess
 import codecs
+from pathlib import Path
 
 from wsl_usb_gui.version import version_scheme
 
@@ -24,36 +26,50 @@ with open("README.md") as f:
     long_description = f.read()
 
 
-setup(
-    name="wsl-usb-gui",
-    author="Andrew Leech",
-    author_email="andrew@alelec.net",
-    url="https://gitlab.com/alelec/wsl-usb-gui",
-    description="WSL USB Management Tool.",
-    long_description=long_description,
-    packages=find_packages(),
-    include_package_data=True,
-    use_scm_version=version_scheme,
-    use_windows_entry_exe=True,
-    setup_requires=["setuptools-scm", "windows-entry-exe"],
-    install_requires=["setuptools-scm", "appdirs"],
-    package_data={
-        "": ["*.txt", "*.rst", "*.md", "*.ico"],
-    },
-    entry_points={
-        "gui_scripts": [
-            "WSL USB GUI = wsl_usb_gui.gui:main",
-        ],
-        "console_scripts": [
-            "WSL USB GUI (debug) = wsl_usb_gui.gui:main",
-        ],
-    },
-    cmdclass={"sdist": sdist},
-    options={
-        "app": {
-            "formal_name": "WSL USB",
-            "bundle": "net.alelec.wsl_usb_gui",
-            "icon": "wsl_usb_gui/usb",
+root = Path(__file__).parent
+pyproject = Path(root / "pyproject.toml")
+pyproject_disabled = Path(root / "__pyproject.toml")
+renamed = False
+
+if pyproject.exists():
+    pyproject.rename(pyproject_disabled)
+    os.system(f"git update-index --assume-unchanged {pyproject}")
+    renamed = True
+
+try:
+    setup(
+        name="wsl_usb_gui",
+        author="Andrew Leech",
+        author_email="andrew@alelec.net",
+        url="https://gitlab.com/alelec/wsl-usb-gui",
+        description="WSL USB Management Tool.",
+        long_description=long_description,
+        packages=find_packages(),
+        include_package_data=True,
+        use_scm_version=version_scheme,
+        use_windows_entry_exe=True,
+        setup_requires=["setuptools-scm", "windows-entry-exe"],
+        install_requires=["setuptools-scm", "appdirs"],
+        package_data={
+            "": ["*.txt", "*.rst", "*.md", "*.ico"],
         },
-    },
-)
+        entry_points={
+            "gui_scripts": [
+                "WSL USB=wsl_usb_gui.gui:main",
+            ],
+            "console_scripts": [
+               "WSL USB debug=wsl_usb_gui.gui:main",
+            ],
+        },
+        cmdclass={"sdist": sdist},
+        options={
+            "app": {
+                "formal_name": "WSL USB",
+                "bundle": "net.alelec.wsl-usb-gui",
+                "icon": "wsl_usb_gui/usb",
+            },
+        },
+    )
+finally:
+    if renamed and pyproject_disabled.exists():
+        pyproject_disabled.rename(pyproject)
